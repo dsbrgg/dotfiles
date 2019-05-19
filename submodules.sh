9f0b6ff21739ec0fa9b91a43fb64571343945133
opuzzz <dsbrgg@gmail.com>
2019-05-19 14:50:52 +0100

    add nerdtree plugin

--- submodules.sh
+++ submodules.sh
@@ -1,4 +1,5 @@
 git submodule add https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh &&
 git submodule add https://github.com/ayu-theme/ayu-vim.git .vim/plugged/ayu-vim &&
 git submodule add https://github.com/Yggdroot/indentLine.git .vim/plugged/indentLine &&
-git submodule add https://github.com/liuchengxu/space-vim-dark.git .vim/plugged/space-vim-dark
+git submodule add https://github.com/liuchengxu/space-vim-dark.git .vim/plugged/space-vim-dark &&
+git submodule add https://github.com/scrooloose/nerdtree.git .vim/plugged/nerdtree
