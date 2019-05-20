d2c2416295fc64fe7da9c3094ea3d9670b817a59
Diego Braga <dsbrgg@gmail.com>
2019-05-20 11:11:49 +0100

    separate os folders

--- submodules.sh
+++
@@ -1,6 +0,0 @@
-git submodule add https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh &&
-git submodule add https://github.com/ayu-theme/ayu-vim.git .vim/plugged/ayu-vim &&
-git submodule add https://github.com/Yggdroot/indentLine.git .vim/plugged/indentLine &&
-git submodule add https://github.com/liuchengxu/space-vim-dark.git .vim/plugged/space-vim-dark &&
-git submodule add https://github.com/scrooloose/nerdtree.git .vim/plugged/nerdtree &&
-git submodule add https://github.com/elzr/vim-json .vim/plugged/vim-json
